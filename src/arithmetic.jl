f62ecf6fc74e81b02c7449a3e6e2472535b7a30f
scheinerman <ers@jhu.edu>
2020-07-22 17:48:05 -0400

    Bug fixing

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -7,20 +7,20 @@ export monic
 
 function (+)(p::SimplePolynomial, q::SimplePolynomial)
     n = max(degree(p), degree(q))
-    T = typeof(p(1)+q(1))
-    c = zeros(T,n+1)
-    for j=0:n
+    T = typeof(p(1) + q(1))
+    c = zeros(T, n + 1)
+    for j = 0:n
         c[j+1] = p[j] + q[j]
     end
     return SimplePolynomial(c)
 end
 
 # polynomial + number
-function (+)(p::SimplePolynomial, a::T) where T<:CoefX
+function (+)(p::SimplePolynomial, a::T) where {T<:CoefX}
     return p + SimplePolynomial(a)
 end
 
-function (+)(a::T, p::SimplePolynomial) where T<:CoefX
+function (+)(a::T, p::SimplePolynomial) where {T<:CoefX}
     return p + SimplePolynomial(a)
 end
 
@@ -31,33 +31,33 @@ function (-)(p::SimplePolynomial)
     return SimplePolynomial(coefs)
 end
 
-(-)(p::SimplePolynomial,q::SimplePolynomial) = p + (-q)
-(-)(p::SimplePolynomial,a::T) where T<:CoefX = p + (-a)
-(-)(a::T,p::SimplePolynomial) where T<:CoefX = a + (-p)
+(-)(p::SimplePolynomial, q::SimplePolynomial) = p + (-q)
+(-)(p::SimplePolynomial, a::T) where {T<:CoefX} = p + (-a)
+(-)(a::T, p::SimplePolynomial) where {T<:CoefX} = a + (-p)
 
 
 # Multiplication
 
-function (*)(a::T, p::SimplePolynomial) where T<:CoefX
-    coefs = [a*x for x in p.data]
+function (*)(a::T, p::SimplePolynomial) where {T<:CoefX}
+    coefs = [a * x for x in p.data]
     return SimplePolynomial(coefs)
 end
 
-(*)(p::SimplePolynomial,a::T) where T<:CoefX = a*p
+(*)(p::SimplePolynomial, a::T) where {T<:CoefX} = a * p
 
-function (*)(p::SimplePolynomial,q::SimplePolynomial)
-    n = degree(p)+degree(q)
+function (*)(p::SimplePolynomial, q::SimplePolynomial)
+    n = degree(p) + degree(q)
 
-    if p==0 || q==0
+    if p == 0 || q == 0
         return SimplePolynomial(0)
     end
 
-    coefs = [ p[0]*q[0] for i=0:n ]
+    coefs = [p[0] * q[0] for i = 0:n]
 
-    for k=1:n
-        coefs[k+1] = 0*coefs[k+1]  # clear it out
-        for j=0:k
-            coefs[k+1] += p[j]*q[k-j]
+    for k = 1:n
+        coefs[k+1] = 0 * coefs[k+1]  # clear it out
+        for j = 0:k
+            coefs[k+1] += p[j] * q[k-j]
         end
     end
     return SimplePolynomial(coefs)
@@ -66,39 +66,39 @@ end
 
 # Integer exponentiation
 
-function (^)(p::SimplePolynomial, k::S) where S<:Integer
-    @assert k>=0 "Exponent must be nonnegative [$k]"
-    if k==0
+function (^)(p::SimplePolynomial, k::S) where {S<:Integer}
+    @assert k >= 0 "Exponent must be nonnegative [$k]"
+    if k == 0
         return SimplePolynomial(Int(1))
     end
 
-    if k==1
+    if k == 1
         return p
     end
 
-    if k==2
-        return p*p
+    if k == 2
+        return p * p
     end
 
-    if k%2 == 0  # even exponent
-        j = k÷2
+    if k % 2 == 0  # even exponent
+        j = k ÷ 2
         q = p^j
-        return q*q
+        return q * q
     end
 
-    j = k÷2
+    j = k ÷ 2
     q = p^j
-    return q*q*p
+    return q * q * p
 end
 
 
 # Division
 
-function (/)(p::SimplePolynomial, a::T) where T<:CoefX
+function (/)(p::SimplePolynomial, a::T) where {T<:CoefX}
     coefs = p.data .// a
     return SimplePolynomial(coefs)
 end
-(//)(p::SimplePolynomial, a::T) where T<:CoefX = p/a
+(//)(p::SimplePolynomial, a::T) where {T<:CoefX} = p / a
 
 """
 `monic(p::SimplePolynomial)` returns a new polynomial formed by
@@ -107,23 +107,25 @@ yielding a monic polynomial. If `p` is zero, a zero polynomial
 is returned.
 """
 function monic(p::SimplePolynomial)::SimplePolynomial
-    if p==0
+    if p == 0
         return p
     end
-    return p/lead(p)
+    return p / lead(p)
 end
 
 
 function divrem(a::SimplePolynomial, b::SimplePolynomial)
-    if b==0
+    if b == 0
         DivideError()
     end
 
-    # DEBUG #
-    println("a = $(a.data) with element type $(eltype(a))")
-    println("b = $(b.data) with element type $(eltype(b))")
-
-    ST = typeof((a[0]+b[0])//1)
+    #ST = typeof((a[0]+b[0])//1)
+    ST = Int
+    if eltype(a) <: Mod || eltype(b) <: Mod
+        ST = eltype(a)
+    else
+        ST = promote_type(eltype(a), eltype(b))
+    end
 
     da = degree(a)
     db = degree(b)
@@ -134,12 +136,12 @@ function divrem(a::SimplePolynomial, b::SimplePolynomial)
 
     leader = a.data[end] // b.data[end]
 
-    coefs = zeros(ST,da-db+1)
+    coefs = zeros(ST, da - db + 1)
     coefs[end] = leader
 
     q = SimplePolynomial(coefs)
-    aa = a - b*q
-    qq,r = divrem(aa,b)
+    aa = a - b * q
+    qq, r = divrem(aa, b)
 
-    return q+qq,r
+    return q + qq, r
 end
